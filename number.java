9ce9bde32ff5720b114ad00ae3fc7484e6ced988
prabumohanM <prabu777rock@gmail.com>
2017-06-20 15:54:45 +0530

    Create number.java

---
+++ number.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.;
+import java.util.Scanner;
+class Sample {
+public static void main(String args[])
+{
+int Number = 0;
+Scanner s = new Scanner(System.in);
+System.out.println("Enter the Numbers :");
+Number = s.nextInt();
+if(Number > 0)
+{
+	System.out.println("The user enter number is positive");
+}
+else if(Number < 0)
+{
+	System.out.println("The user enter number is negetive");
+}
+else
+	System.out.println("The user enter number is Zero");
+}
+}
