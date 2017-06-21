57f20301abcc51f2c20629227b2ad6aff46a23de
prabumohanM <prabu777rock@gmail.com>
2017-06-21 10:27:23 +0530

    Create alpha.java

---
+++ alpha.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+public class Hello
+{
+public static void main(String args[])
+{
+char ch;
+Scanner s = new Scanner(System.in);
+System.out.println("Enter the Characters :");
+ch = s.next().charAt(0);
+if((ch>='a' && ch<='z')||(ch>='A' && ch<='Z'))
+{
+	System.out.println("Entered char "+ch+" is Alphabet");
+}
+else
+{
+	System.out.println("Entered char "+ch+" is NotAlphabet");
+}
+}
+}
