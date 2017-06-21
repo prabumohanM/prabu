a7e589dcc1a0afc39c0ce312177e3d1493b6aba1
prabumohanM <prabu777rock@gmail.com>
2017-06-21 12:10:56 +0530

    Update natural.java

--- natural.java
+++ natural.java
@@ -1,18 +1,21 @@
-  import java.util.*;
+import java.util.*;
 import java.lang.*;
 import java.io.*;
 import java.util.Scanner;
-class Hello
+public class Natural 
 {
-public static void main(String args[])
-{
-        int i;
-	System.out.println ("The first 100 natural numbers are:\n");
-	for (i=1;i<=100;i++)
-	{      
-		System.out.println (i);
-	}
-System.out.println ("\n");
-}
-}
+    public static void main(String args[])
+    {
+        int x, i = 1 ;
+        int sum = 0;
+        System.out.println("Enter Number of items :");
+        Scanner s = new Scanner(System.in);
+        x = s.nextInt();
+        while(i <= x)
+        {
+            sum = sum +i;
+            i++;
+        }
+        System.out.println("Sum of "+x+" numbers is :"+sum);
+    } 
 }
