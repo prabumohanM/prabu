ccdbe7716dfa5b00b3168a1a894371caea6077a7
prabumohanM <prabu777rock@gmail.com>
2017-06-21 12:07:53 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,18 @@
+  import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Hello
+{
+public static void main(String args[])
+{
+        int i;
+	System.out.println ("The first 100 natural numbers are:\n");
+	for (i=1;i<=100;i++)
+	{      
+		System.out.println (i);
+	}
+System.out.println ("\n");
+}
+}
+}
