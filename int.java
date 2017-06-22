1d3ca11dd4a5fb287507ee4c9cd3522a6a5497e8
prabumohanM <prabu777rock@gmail.com>
2017-06-22 11:42:34 +0530

    Create int.java

---
+++ int.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Hello
+{
+public static void main(String args[])
+{
+ String S1 = "100";
+	   int I1 = Integer.parseInt(S1);
+	   System.out.println("The Actual String is:"+S1);
+	   System.out.println("The Converted to Int is:"+I1);
+	   System.out.println("The Arithmetic o/p is:"+(I1/4));
+}
+}
+
