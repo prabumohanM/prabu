447ad3cc8b1cb27093a02d05624130fdf674d9ab
prabumohanM <prabu777rock@gmail.com>
2017-06-22 12:36:37 +0530

    Create evenoddsting.java

---
+++ evenoddsting.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Hello
+{
+public static void main(String args[])
+{
+string input = "AXBYCZ"; //Sample String
+StringBuilder output = new StringBuilder();
+ 
+char[] characters = input.ToCharArray();
+ 
+for (int i = 0; i < characters.Length; i++)
+{
+  if (i % 2 == 0)
+  {
+    if((i+1) < characters.Length )
+    {
+      output.Append(characters[i + 1]);
+    }
+               output.Append(characters[i]);
+  }
+}
+}
+}
